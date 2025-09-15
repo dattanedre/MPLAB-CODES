6696a1af71c6f09b64d1b475041fb1f5a606386d
Datta Nedre <157895346+dattanedre@users.noreply.github.com>
2025-09-15 20:05:11 +0530

    Switch
    C and Hex file of Switch

---
+++ switch_1.c
@@ -0,0 +1,77 @@
+/*#include<p18f4580.h>
+
+#define led PORTCbits.RC0
+
+#define sw PORTCbits.RC1
+#define ON 1
+#define OFF 0
+void delay(int a);
+int main()
+{
+
+TRISCbits.RC0=0;
+TRISCbits.RC1=1;
+
+
+while(1)
+{
+if(sw==0)
+{
+led=ON;
+}
+else
+{led=OFF;
+}
+
+}
+}
+void delay(int a)
+{
+int i,j;// do not write int in for loop
+for(i=0;i<a;i++)
+{
+for(j=0;j<a;j++)
+{
+
+}
+}
+}
+*/
+
+#include<p18f4580.h>
+
+#define led PORTCbits.RC0
+
+#define sw PORTCbits.RC1
+
+void delay(int a);
+int main()
+{
+
+TRISCbits.RC0=0;
+TRISCbits.RC1=1;
+
+
+while(1)
+{
+if(sw==0)
+{
+led=1;
+}
+else
+{led=0;
+}
+
+}
+}
+void delay(int a)
+{
+int i,j;// do not write int in for loop
+for(i=0;i<a;i++)
+{
+for(j=0;j<a;j++)
+{
+
+}
+}
+}
